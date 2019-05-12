92f2cca6edccfadf29eea0650ef849638aee32f6
Roman <r.poliakh@gmail.com>
2019-05-12 19:51:15 +0300

    - added svg sprites

--- src/script/script.js
+++ src/script/script.js
@@ -15,61 +15,57 @@ function menuHover(event){
 	}
 }
 // --Menu burger end--
-
 //-------------------------------------------------------------------
 
 
-function ajaxSelect(wrapElem,card,link){
+function AjaxSelect(wrapElem,card){
 	let myCards = document.createDocumentFragment();
 	let parentCards = document.querySelector(wrapElem);
 	let elemCard = parentCards.querySelector(card);
 
-	elemCard.hidden = false;
-	let showLoading = function(on){
+	let showLoading = (on)=>{
 		let loader = document.querySelector('.loader');
-		(on)? loader.classList.add('loaderHide'):loader.classList.remove('loaderHide')
+		(on)? loader.classList.remove('loaderHide'):loader.classList.add('loaderHide')
 	}
-	function checkStatus(response) {
-		if (response.status >= 200 && response.status < 300) {
-			return response
-		} else {
+	let checkStatus = (response)=>{
+		if (response.ok) {
+			return response.json()
+		}else{
 			showLoading(false);
-			var error = new Error("Ошибка " + response.statusText);
-			// error.response = response;
-			// throw error;
+			let error = new Error("Ошибка " + response.statusText);
+			error.response = response;
+			throw error;
 		}
 	}
-	function createCard (obj){
+	let createCard = (obj)=>{
 		let newCard = elemCard.cloneNode(true);
+		newCard.hidden = false;
 		newCard.querySelector('.card__page').href = obj.gitpage;
 		newCard.querySelector('.card__hub').href = obj.github;
 		newCard.style.backgroundImage = `url("${obj.preview}")`;
 		return newCard;
 	}
-	function parseJSON (obj){
+	let parseJSON = (obj)=>{
 		for (let key in obj){
-			
 			myCards.appendChild(createCard(obj[key]));
 		}
-		parentCards.appendChild(myCards);	
-		return obj
+			showLoading(false);
+			parentCards.appendChild(myCards);	
 	}
-	return {
-		load : function(){
-					showLoading(true);
-					while(parentCards.querySelector(card)){
-						parentCards.removeChild(parentCards.querySelector(card))
-					}
-					fetch(link)
-					.then(checkStatus)
-					.then(response => response.json())
-					.then(parseJSON)
-					.catch(function(error) {
-						showLoading(false);
-						alert('request failed', error)
-					})
-				}
+	this.load = (url)=>{
+		showLoading(true);
+		while(parentCards.querySelector(card)){
+			parentCards.removeChild(parentCards.querySelector(card))
+		}
+		fetch(url)
+		.then(checkStatus)
+		.then(parseJSON)
+		.catch((error)=>{
+			showLoading(false);
+			alert('request failed', error)
+		})
 	}
 }
-let select = ajaxSelect('.cardWrap', '.card','ololo.json' )
-select.load();
+
+let ajaxSelect = new AjaxSelect('.cardWrap', '.card')
+ajaxSelect.load('ololo.json');
